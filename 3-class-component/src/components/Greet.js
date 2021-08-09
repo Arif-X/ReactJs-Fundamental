7222a6b708a2b11d45ff544074d01920e0549e27
Arif-X <ariffudinnotresponding@gmail.com>
2021-08-09 21:43:37 +0700

    Class Component

---
+++ 3-class-component/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Greet extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is Class Component</h2>
+            </div>
+        )
+    }
+}
+
+export default Greet
