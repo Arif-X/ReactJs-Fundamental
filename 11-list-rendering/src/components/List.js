a162f0d14b7173674a1395e67c7c49d1c66e3bf2
Arif-X <ariffudinnotresponding@gmail.com>
2021-08-11 02:32:01 +0700

    List Rendering

--- 11-list-rendering/src/components/List.js
+++ 11-list-rendering/src/components/List.js
@@ -20,7 +20,7 @@ function List() {
         }
     ]
     // const data = array.map(array => <h2>Hello, I'm {array.name}. My javascript framework skill is {array.skill}.</h2>)
-    const data = array.map(array => <Data />)
+    const data = array.map(array => <Data name={array.name} skill={array.skill}/>)
     return (
         <div>
             {data}
