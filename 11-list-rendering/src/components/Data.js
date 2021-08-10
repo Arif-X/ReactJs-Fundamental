0eb6192d28622721374ee86a0c540b5207b79fc8
Arif-X <ariffudinnotresponding@gmail.com>
2021-08-11 02:19:02 +0700

    List Rendering

---
+++ 11-list-rendering/src/components/Data.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Data(props) {
+    return (
+        <div>
+            <h2>Hello, I'm {props.name}. My javascript framework skill is {props.skill}.</h2>
+        </div>
+    )
+}
+
+export default Data
---
+++ 11-list-rendering/src/components/List.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Data from './Data'
+
+function List() {
+    const array = [
+        {
+            id: '1',
+            name: 'Arif',
+            skill: 'ReactJs'
+        },
+        {
+            id: '2',
+            name: 'Ari',
+            skill: 'VueJs'
+        },
+        {
+            id: '3',
+            name: 'Fira',
+            skill: 'Angular'
+        }
+    ]
+    // const data = array.map(array => <h2>Hello, I'm {array.name}. My javascript framework skill is {array.skill}.</h2>)
+    const data = array.map(array => <Data />)
+    return (
+        <div>
+            {data}
+        </div>
+    )
+}
+
+export default List
